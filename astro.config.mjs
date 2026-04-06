a1495c2c89e94084c6bbd599ea565813c5d67ff9
ZhenYu Hu <huxxshadowhzy@gmail.com>
2026-04-06 18:15:09 +0800

    Add Tailwind CSS integration and global styles and Font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://huxxshadow.github.io',
-    base: '/huxx',
+  site: 'https://huxxshadow.github.io',
+  base: '/huxx',
 
-
-
-
-
-
-
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
