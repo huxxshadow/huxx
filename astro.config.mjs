6a94652de33da58deaecf22d861239ff9e99ca3d
ZhenYu Hu <huxxshadowhzy@gmail.com>
2026-05-13 23:56:46 +0800

    Update base path in Astro configuration for correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://huxxshadow.github.io',
-  // base: '/huxx',
+  base: '/',
 
   vite: {
     plugins: [tailwindcss()],
